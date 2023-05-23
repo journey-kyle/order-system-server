b5c65b9c53e1e854ee313c81918e25dfa0baf4e4
journey-kyle <proextremer@gmail.com>
2023-05-23 20:02:00 +0900

    기본 셋팅 완료

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const conn = require('mysql2').createPool({
+        host:process.env.DB_URL,
+        user:process.env.DB_ID,
+        password:process.env.DB_PW,
+        database:process.env.DB_NAME
+    }
+)
+
+app.use(express.json());
+
+var cors = require('cors');
+var parser = require('xml2json');
+const path = require('path');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.use(cors());
+
+app.listen(process.env.PORT, function(error, result, field){
+    if(error) console.log(error)
+    else{
+        console.log(parser);
+        console.log("Listen on port : " + process.env.PORT);
+        console.log("DB_URL : " + process.env.DB_URL);
+        console.log("DB_NAME : " + process.env.DB_NAME);
+        console.log('result : ' + field);
+    }
+});
+
+app.get('/', function(req, res){
+    res.send("hello");
+});
